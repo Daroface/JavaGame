daeaa7866180323d259172d1150c3c9ee4b555e2
daroface <daroface@daroface>
2018-11-02 11:12:19 +0100

    init commit

---
+++ JavaGame/src/pl/dominik/dziembala/Game.java
@@ -0,0 +1,13 @@
+package pl.dominik.dziembala;
+
+import pl.dominik.dziembala.controller.GameController;
+
+public class Game {
+	
+	public static void main(String[] args) {
+		GameController gameController = new GameController();
+		gameController.run();
+		
+	}
+
+}
---
+++ JavaGame/src/pl/dominik/dziembala/controller/GameController.java
@@ -0,0 +1,15 @@
+package pl.dominik.dziembala.controller;
+
+
+
+public class GameController {
+	
+	public GameController() {
+		
+	}
+	
+	public void run() {
+		
+	}
+
+}
