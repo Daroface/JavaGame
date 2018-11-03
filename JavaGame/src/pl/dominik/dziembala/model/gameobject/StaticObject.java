e8000848ad3fa7eb436f75f033894d653160ea6c
daroface <daroface@daroface>
2018-11-03 16:04:49 +0100

    Added class which represents all static objects

--- JavaGame/src/pl/dominik/dziembala/model/gameobject/StaticObject.java
+++ JavaGame/src/pl/dominik/dziembala/model/gameobject/StaticObject.java
@@ -1,12 +1,13 @@
 package pl.dominik.dziembala.model.gameobject;
 
 import java.awt.Color;
+import java.awt.geom.Rectangle2D;
 
 public class StaticObject extends GameObject {
 
 	public StaticObject(Color color, int xPosition, int yPosition, int width, int height) {
 		super(color, xPosition, yPosition, width, height);
-		// TODO Auto-generated constructor stub
+		this.shape = new Rectangle2D.Double(this.xPosition, this.yPosition, this.width, this.height);
 	}
 
 	@Override
