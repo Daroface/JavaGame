8d9799957ca592ebf1757474c65f75b1ef3d5cff
daroface <daroface@daroface>
2018-11-03 15:39:17 +0100

    Changed from interface to abstract class

--- JavaGame/src/pl/dominik/dziembala/model/gameobject/GameObject.java
+++ JavaGame/src/pl/dominik/dziembala/model/gameobject/GameObject.java
@@ -1,11 +1,37 @@
 package pl.dominik.dziembala.model.gameobject;
 
+import java.awt.Color;
 import java.awt.Shape;
 
-public interface GameObject {
+public abstract class GameObject {
 	
-	public Shape getShape();
+	protected int xPosition;
 	
-	public void moveX();
-
+	protected int yPosition;
+	
+	protected int width;
+	
+	protected int height;
+	
+	protected Color color;
+	
+	protected Shape shape;
+	
+	protected GameObject(Color color, int xPosition, int yPosition, int width, int height) {
+		this.color = color;
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Shape getShape() {
+		return shape;
+	}
+	
+	public abstract void move();
+	
+	public Color getColor() {
+		return color;
+	}
 }
