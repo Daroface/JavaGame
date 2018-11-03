751aff7eb8c9674ca03beb62eeef6aa897629a2e
daroface <daroface@daroface>
2018-11-03 16:05:18 +0100

    Added class which represents all obstacle objects in game

---
+++ JavaGame/src/pl/dominik/dziembala/model/gameobject/ObstacleObject.java
@@ -0,0 +1,29 @@
+package pl.dominik.dziembala.model.gameobject;
+
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+
+public class ObstacleObject extends GameObject {
+
+	public int speed;
+
+	protected ObstacleObject(Color color, int xPosition, int yPosition, int width, int height, int speed) {
+		super(color, xPosition, yPosition, width, height);
+		shape = new Rectangle2D.Double(xPosition, yPosition, width, height);
+		this.speed = speed;
+	}
+
+	@Override
+	public void move() {
+		if ((xPosition + width) > 0) {
+			xPosition -= speed;
+			shape = null;
+			shape = new Rectangle2D.Double(xPosition, yPosition, width, height);
+		}
+	}
+
+	@Override
+	public void jump() {
+	}
+
+}
