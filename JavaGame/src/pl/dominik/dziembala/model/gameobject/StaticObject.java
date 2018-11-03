9c4bb86678019a16a6e910e735f936bb8d76e359
daroface <daroface@daroface>
2018-11-03 15:51:48 +0100

    Created static type game object class

---
+++ JavaGame/src/pl/dominik/dziembala/model/gameobject/StaticObject.java
@@ -0,0 +1,20 @@
+package pl.dominik.dziembala.model.gameobject;
+
+import java.awt.Color;
+
+public class StaticObject extends GameObject {
+
+	public StaticObject(Color color, int xPosition, int yPosition, int width, int height) {
+		super(color, xPosition, yPosition, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void move() {		
+	}
+
+	@Override
+	public void jump() {		
+	}
+
+}
