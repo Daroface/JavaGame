41bbb748b40b6d6c44b5a364eeeb3bd5c7cc653e
daroface <daroface@daroface>
2018-11-03 15:53:15 +0100

    Added floor and sky to static's game objects class

--- JavaGame/src/pl/dominik/dziembala/model/gameobject/Sky.java
+++ JavaGame/src/pl/dominik/dziembala/model/gameobject/Sky.java
@@ -3,17 +3,10 @@ package pl.dominik.dziembala.model.gameobject;
 import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 
-public class Sky extends GameObject {
+public class Sky extends StaticObject {
 
 	public Sky(Color color, int xPosition, int yPosition, int width, int height) {
 		super(color, xPosition, yPosition, width, height);
 		this.shape = new Rectangle2D.Double(this.xPosition, this.yPosition, this.width, this.height);
 	}
-
-	@Override
-	public void move() {
-		
-	}
-	
-
 }
