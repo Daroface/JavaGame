f387e5b4eb375674c2ed30f1495c0b1b67a098b6
daroface <daroface@daroface>
2018-11-05 18:42:54 +0100

    Added method which represents jumping and method to detect collision

--- JavaGame/src/pl/dominik/dziembala/model/gameobject/Player.java
+++ JavaGame/src/pl/dominik/dziembala/model/gameobject/Player.java
@@ -4,26 +4,96 @@ import java.awt.Color;
 import java.awt.geom.*;
 
 public class Player extends GameObject {
-		
-	private int life;
-	private static final int MAXJUMPAMOUNT = 2;
-	private int jumpAmount;
-	private static final int JUMPVALUE = 50;
-	private int speed = 1;
-	
+
+	private int jumpAmount = 0;
+	private static final int JUMPVALUE = 70;
+	private static final double JUMPWIDTH = 130.0;
+	private double jumpWay = 0.0;
+	private double doubleJumpWay = 0.0;
+	private double speed = 1.0;
+	private int normalYPosition;
+	private int actualYPosition;
+
 	public Player(Color color, int xPosition, int yPosition, int diameter) {
 		super(color, xPosition, yPosition, diameter, diameter);
+		normalYPosition = yPosition;
+		actualYPosition = yPosition;
 		shape = new Ellipse2D.Double(this.xPosition, this.yPosition, this.width, this.height);
-		life = 1;
-		jumpAmount = 0;
 	}
-		
+
 	@Override
 	public void move() {
-		if(xPosition < 200) {
+		if (xPosition < 200) {
 			xPosition += speed;
-			shape = new Ellipse2D.Double(xPosition, yPosition, width, height);	
-		}			
+			shape = null;
+			shape = new Ellipse2D.Double(xPosition, yPosition, width, height);
+		}
+	}
+
+	public void jump() {
+		switch (jumpAmount) {
+		case 1: {
+			if (jumpWay / JUMPWIDTH != 1.0) {
+				jumpWay += 1.0;
+				yPosition = normalYPosition
+						- (int) (JUMPVALUE * Math.sin((jumpWay / JUMPWIDTH * Math.toRadians(180.0))));
+			} else {
+				jumpWay = 0.0;
+				jumpAmount = 0;
+			}
+			break;
+		}
+
+		case 2: {
+			if (yPosition < normalYPosition) {
+				doubleJumpWay += 1.0;
+				yPosition = actualYPosition
+						- (int) ((JUMPVALUE) * Math.sin((doubleJumpWay / (JUMPWIDTH) * Math.toRadians(180.0))));
+			} else {
+				doubleJumpWay = 0.0;
+				jumpWay = 0.0;
+				jumpAmount = 0;
+			}
+		}
+		default: {
+			break;
+		}
+		}
+		if (jumpAmount != 0) {
+
+			shape = null;
+			shape = new Ellipse2D.Double(xPosition, yPosition, width, height);
+		}
+	}
+
+	public void increaseJumpAmount() {
+		switch (jumpAmount) {
+		case 0: {
+			jumpAmount = 1;
+			break;
+		}
+		case 1: {
+			jumpAmount = 2;
+			actualYPosition = yPosition;
+			break;
+		}
+		default: {
+		}
+		}
+	}
+
+	public boolean checkCollision(GameObject obstacle) {
+		double collisionX1Position = this.xPosition + 0.97;
+		double collisionX2Position = this.xPosition + this.width - 0.97;
+		double collisionYPosition = this.yPosition + this.height - 0.97;
+		if ((double) obstacle.yPosition > collisionYPosition)
+			return false;
+		else if ((double) (obstacle.xPosition) > collisionX2Position)
+			return false;
+		else if ((double) (obstacle.xPosition + obstacle.width) <= collisionX1Position)
+			return false;
+		else
+			return true;
 	}
 
 }
