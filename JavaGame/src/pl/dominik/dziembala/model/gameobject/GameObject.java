deaa986cbb7d6fee8573c250263997b629f502af
daroface <daroface@daroface>
2018-11-05 18:44:08 +0100

    Added getter to width's field

--- JavaGame/src/pl/dominik/dziembala/model/gameobject/GameObject.java
+++ JavaGame/src/pl/dominik/dziembala/model/gameobject/GameObject.java
@@ -4,19 +4,19 @@ import java.awt.Color;
 import java.awt.Shape;
 
 public abstract class GameObject {
-	
+
 	protected int xPosition;
-	
+
 	protected int yPosition;
-	
+
 	protected int width;
-	
+
 	protected int height;
-	
+
 	protected Color color;
-	
+
 	protected Shape shape;
-	
+
 	protected GameObject(Color color, int xPosition, int yPosition, int width, int height) {
 		this.color = color;
 		this.xPosition = xPosition;
@@ -24,14 +24,22 @@ public abstract class GameObject {
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	public Shape getShape() {
 		return shape;
 	}
-	
+
 	public abstract void move();
-	
+
 	public Color getColor() {
 		return color;
+	}
+	
+	public int getXPosition() {
+		return xPosition;
+	}
+	
+	public int getWidth() {
+		return width;
 	}
 }
