d791d8f9fa45c1a88846676141000acd83c6eec5
daroface <daroface@daroface>
2018-11-06 15:41:10 +0100

    Corrected playing music in loop

--- JavaGame/src/pl/dominik/dziembala/model/MusicPlayer.java
+++ JavaGame/src/pl/dominik/dziembala/model/MusicPlayer.java
@@ -1,39 +1,20 @@
 package pl.dominik.dziembala.model;
 
-import java.awt.Toolkit;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import javafx.application.Application;
-import javafx.scene.media.AudioClip;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
-import sun.audio.AudioData;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-import sun.audio.ContinuousAudioDataStream;
-
 
 public class MusicPlayer {
 
-	private String name;
+	private volatile String name;
 	AudioInputStream audioInputStream;
 	Clip clip;
 	
 	public MusicPlayer(String name) throws Exception {
 		this.name = name;		
+		System.out.println(this.name);
 		audioInputStream = AudioSystem.getAudioInputStream(new File(this.name));
 		clip = AudioSystem.getClip();
 		clip.open(audioInputStream);		
